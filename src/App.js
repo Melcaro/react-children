6d6a0fcc17f83317e37f96fa961e47f118baf5a7
melanie caro <melanie.caro@stackerine.com>
2019-11-15 10:22:51 +0100

    made a button with a picture inside

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ButtonComponent } from './ButtonComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonComponent>
+        <span className="betaText">BETA</span>
+        <img
+          alt="delicious cookies"
+          src="https://images.unsplash.com/photo-1499636136210-6f4ee915583e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+        />
+      </ButtonComponent>
     </div>
   );
 }
---
+++ src/ButtonComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ButtonComponent = ({ children }) => (
+  <div>
+    <button>{children}</button>
+  </div>
+);
+
+export { ButtonComponent };
